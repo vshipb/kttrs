f6c6ad2147fc862d1ae078cce12ee9933c6c6db2
Valery Shorin <valery.a.shorin@gmail.com>
2025-08-30 14:55:10 +0300

    feat: Add new block sprites

--- app/src/main/java/com/example/kttrs/Piece.kt
+++ app/src/main/java/com/example/kttrs/Piece.kt
@@ -10,13 +10,13 @@ enum class PieceType(
     private val baseShape: List<List<Int>>,
     override val drawableResId: Int
 ) : PieceSpec {
-    I(listOf(listOf(1, 1, 1, 1)), R.drawable.block_i),
-    O(listOf(listOf(1, 1), listOf(1, 1)), R.drawable.block_o),
-    T(listOf(listOf(0, 1, 0), listOf(1, 1, 1)), R.drawable.block_t),
-    S(listOf(listOf(0, 1, 1), listOf(1, 1, 0)), R.drawable.block_s),
-    Z(listOf(listOf(1, 1, 0), listOf(0, 1, 1)), R.drawable.block_z),
-    J(listOf(listOf(1, 0, 0), listOf(1, 1, 1)), R.drawable.block_j),
-    L(listOf(listOf(0, 0, 1), listOf(1, 1, 1)), R.drawable.block_l);
+    I(listOf(listOf(1, 1, 1, 1)), R.drawable.sblock_i),
+    O(listOf(listOf(1, 1), listOf(1, 1)), R.drawable.sblock_o),
+    T(listOf(listOf(0, 1, 0), listOf(1, 1, 1)), R.drawable.sblock_t),
+    S(listOf(listOf(0, 1, 1), listOf(1, 1, 0)), R.drawable.sblock_s),
+    Z(listOf(listOf(1, 1, 0), listOf(0, 1, 1)), R.drawable.sblock_z),
+    J(listOf(listOf(1, 0, 0), listOf(1, 1, 1)), R.drawable.sblock_j),
+    L(listOf(listOf(0, 0, 1), listOf(1, 1, 1)), R.drawable.sblock_l);
 
     override val shape: List<List<Int>>
         get() = baseShape
